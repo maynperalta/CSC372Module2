40624160410409c744a9e06537b3b3a96170d82a
Maynard Peralta <maynperalta@gmail.com>
2025-11-21 21:28:08 -0700

    Begin coding GUI frame

---
+++ CSC372Module2/src/main/UserAccountFrame.java
@@ -0,0 +1,55 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class UserAccountFrame extends JFrame implements ActionListener {
+    
+	private JLabel userBalanceLabel;
+    private JTextField balanceField;
+    private JLabel userDepositLabel;
+    private JTextField depositField;
+    private JLabel userWithdrawLabel;
+    private JTextField withdrawField;
+    private JButton balanceBtn;
+    private JButton depositBtn;
+    private JButton withdrawBtn;
+	
+	UserAccountFrame() {
+        GridBagConstraints positionConst = null;
+        	
+        setTitle("User Account Information");
+        	
+        userBalanceLabel = new JLabel("Please Enter Your Balance.");
+        balanceField = new JTextField(15);
+        balanceField.setEditable(true);
+        balanceField.setText("balance");
+        
+        balanceBtn = new JButton("Submit Balance");
+        balanceBtn.addActionListener(this);
+        	
+        setLayout(new GridBagLayout());
+        positionConst = new GridBagConstraints();
+        	
+        positionConst.gridx = 2;
+        positionConst.gridy = 0;
+        positionConst.insets = new Insets (10, 10, 10, 10);
+        add(userBalanceLabel, positionConst);
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent event) {
+        	String userInput;
+        	int userBalance;
+        	
+        	userInput = balanceField.getText();
+        	userBalance = Integer.parseInt(userInput);
+        	
+        	
+        }
+	public static void main(String[] args) {
+	
+	
+	}
+}

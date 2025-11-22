23bc2a3d7e99cab3d64786a3b57eb265b467e58f
Maynard Peralta <maynperalta@gmail.com>
2025-11-22 10:53:29 -0700

    added comments

--- CSC372Module2/src/main/UserAccountFrame.java
+++ CSC372Module2/src/main/UserAccountFrame.java
@@ -20,24 +20,24 @@ public class UserAccountFrame extends JFrame implements ActionListener {
     private JLabel confirmBalanceLabel;
 	
 	UserAccountFrame() {
-        GridBagConstraints positionConst = null;
+        GridBagConstraints positionConst;
         	
         setTitle("User Account Information");
-        
+// set number format for input fields and eliminate comma separator        
         NumberFormat format = NumberFormat.getNumberInstance();
         format.setGroupingUsed(false);
-        	
+// balance input         	
         userBalanceLabel = new JLabel("Please Enter Your Balance.");
         balanceField = new JFormattedTextField(format);
         balanceField.setEditable(true);
         balanceField.setText("");
         balanceBtn = new JButton("Submit Balance");
         balanceBtn.addActionListener(this);
-        	
+// layout for using gridbag system        	
         setLayout(new GridBagLayout());
         positionConst = new GridBagConstraints();
         positionConst.insets = new Insets (10, 10, 10, 10);
-               	
+// positioning for balance items               	
         positionConst.gridx = 1;
         positionConst.gridy = 0;
         positionConst.fill = GridBagConstraints.NONE;
@@ -58,7 +58,7 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         
         confirmBalanceLabel = new JLabel("");
         confirmBalanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        
+// balance items in separate panel to avoid confines of frame grid        
         JPanel confirmPanel = new JPanel(new BorderLayout());
         confirmPanel.add(confirmBalanceLabel, BorderLayout.CENTER);
         
@@ -68,9 +68,9 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         positionConst.fill = GridBagConstraints.HORIZONTAL;
         positionConst.weightx = 1.0;       
         add(confirmPanel, positionConst);
-        
+// reset grid width for other items        
         positionConst.gridwidth = 1;
-        
+ // deposit entry        
         userDepositLabel = new JLabel("Deposit Amount: ");
         depositField = new JFormattedTextField(format);
         depositField.setEditable(true);
@@ -95,7 +95,7 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         positionConst.fill = GridBagConstraints.NONE;
         positionConst.weightx = 0;
         add(depositBtn, positionConst);
-        
+// withdrawal entry        
         userWithdrawLabel = new JLabel("Withdraw Amount: ");
         withdrawField = new JFormattedTextField(format);
         withdrawField.setEditable(true);
@@ -120,7 +120,7 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         positionConst.fill = GridBagConstraints.NONE;
         positionConst.weightx = 0;
         add(withdrawBtn, positionConst);
-        
+// hide deposit and withdrawal items until user enters balance        
         userDepositLabel.setVisible(false);
         depositField.setVisible(false);
         depositBtn.setVisible(false);
@@ -129,20 +129,21 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         withdrawBtn.setVisible(false);
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+// set minimum size to ensure items are visible;
         setMinimumSize(new Dimension(400, 200));
         setPreferredSize(new Dimension(400, 200));
-        
+// allow frame to dynamically resize        
 		pack();
 		setVisible(true);
-		
+// popup for final balance when user closes frame		
 		addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				JOptionPane.showMessageDialog(null,  String.format("Goodbye. Your balance is: $%.2f", userBalance));
 			}
 		});
-        }        
+        }     
+// button action event listeners
         @Override
         public void actionPerformed(ActionEvent e) {
         	try {
@@ -150,18 +151,18 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         			userBalance = Double.parseDouble(balanceField.getText());
         			confirmBalanceLabel.setText(String.format("Welcome. Your balance is: $%.2f", userBalance));
         			balanceField.setText("");
-        			
+// make deposit and withdrawal items visible after balance has been entered        			
         			userDepositLabel.setVisible(true);
         	        depositField.setVisible(true);
         	        depositBtn.setVisible(true);
         	        userWithdrawLabel.setVisible(true);
         	        withdrawField.setVisible(true);
         	        withdrawBtn.setVisible(true);
-        	        
+// hide balance items once entered        	        
         	        userBalanceLabel.setVisible(false);
         	        balanceField.setVisible(false);
         	        balanceBtn.setVisible(false);
-        	        
+// refresh frame after updating visibility        	        
         	        this.revalidate();
         	        this.repaint();
         	        pack();
@@ -183,6 +184,7 @@ public class UserAccountFrame extends JFrame implements ActionListener {
         		} 
         	} catch (NumberFormatException ex) {
         		JOptionPane.showMessageDialog(this, "Please enter a valid number.", "Error", JOptionPane.ERROR_MESSAGE);
+// clear fields in the event of an error
         		balanceField.setText("");
         		depositField.setText("");
         		withdrawField.setText("");
